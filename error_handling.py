93945cf56d4b52a51f8f933788536b2813458a41
LuqGon <gonzalezlucasroman@outlook.com>
2022-03-05 14:57:50 -0300

    Control de errores con try-except y raise

---
+++ error_handling.py
@@ -0,0 +1,24 @@
+'''
+Manejo de errores con try, except y raise
+'''
+def divisor(num):
+    try:
+        if num < 0:
+            raise ValueError("ingresa un número positvo")
+        divisors =[i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
+
+
+def main():
+    try: 
+        num = int(input('Ingrese un número: '))      
+        print(divisor(num))
+        print("Termino mi programa")
+    except ValueError:
+        print("El valor ingresado no es valido. Ingrese un numero")
+    
+if __name__ =='__main__':
+    main()
