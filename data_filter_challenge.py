4b118e52f649a34a859e5a22f71ec0fc5fa752b7
LuqGon <gonzalezlucasroman@outlook.com>
2022-03-05 03:59:44 -0300

    script con el challege

--- data_filter_challenge.py
+++ data_filter_challenge.py
@@ -72,21 +72,19 @@ DATA = [
 ]
 
 def main():
-    all_python_devs = [worker['name'] for worker in DATA if worker['language'] == 'python']
+    all_python_devs = list(filter(lambda worker: worker['language'] == 'python',DATA))
+    all_python_devs= list(map(lambda worker: worker['name'],all_python_devs))
     
-    all_platzi_workers = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi']
-
-    adults = list(filter(lambda worker: worker["age"]>18,DATA))
-    adults = list(map(lambda worker: worker["name"],adults))
-
+    all_platzi_workers = list(filter(lambda worker: worker['organization'] == 'Platzi',DATA))
+    all_platzi_workers= list(map(lambda worker: worker['name'],all_platzi_workers))
     
-    old_people = list(map(lambda worker : worker | {"old" : worker["age"]>70},DATA)) # el | permite unir diccionarios
+    adults = [worker["name"] for worker in DATA if worker["age"]>18]
+
+    old_people = [worker | {"old" : worker["age"]>70} for worker in DATA]
    
-
-    for worker in adults:
+   
+    for worker in old_people:
         print(worker)
-
-
 
 if __name__ == '__main__':
     main()
